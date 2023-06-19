bdabe5520a4103ea4f4d6497c136e78b3452bf88
TanyeemSafwan <tanyeemsafwan123@gmail.com>
2023-06-19 12:17:33 +0600

    initial files added

---
+++ resources/views/dbconn.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database Connection</title>
+</head>
+<body>
+    <div>
+        <?php
+            if(DB::connection()->getPdo()) {
+                echo "Successfully connected to ".DB::connection()->getDatabaseName();
+            }
+        ?>
+    </div>
+</body>
+</html>
